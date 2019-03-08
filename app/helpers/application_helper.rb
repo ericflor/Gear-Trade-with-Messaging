f96473e361a44a5d295d3c178801ac9250f152d8
Eric Florence <ericflorence.dev@gmail.com>
2019-03-08 09:56:57 -0800

    update all the views for trades, add helper classes, config routes, this is getting easier every day boi

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  
+  require 'kramdown'
+
+  def gravatar_for(user, options = { size: 200 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "border-radius-25")
+  end
+
+  def markdown_to_html(text)
+    Kramdown::Document.new(text).to_html
+  end
+
+  def trade_author(trade)
+    user_signed_in? && current_user.id == trade.user_id
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 require 'sidekiq/web'
+require 'kramdown'
 
 Rails.application.routes.draw do
   resources :trades
